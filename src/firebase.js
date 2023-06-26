e4985cdd9ff2036e181a9c03b6b223ed3200a91b
Rudrakrishna-Das <rudradas16996@gmail.com>
2023-06-26 18:45:24 +0530

    first add

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB0UQD-LPqIqhsUz12oeTgOHlLoVyaMA0E",
+  authDomain: "netflix-clone-rudra-19307.firebaseapp.com",
+  projectId: "netflix-clone-rudra-19307",
+  storageBucket: "netflix-clone-rudra-19307.appspot.com",
+  messagingSenderId: "1078736451138",
+  appId: "1:1078736451138:web:8f06cd553130ec697b5fdb",
+};
